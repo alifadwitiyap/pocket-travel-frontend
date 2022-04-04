79dc92c56a9316bbfe2ac673aa50327a48597c12
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2022-04-04 15:11:07 +0700

    Fix More component's styling

--- src/components/More/index.js
+++ src/components/More/index.js
@@ -11,17 +11,17 @@ function More({ editHandler, deleteHandler }) {
   };
 
   return (
-    <>
-      <div className="relative p-1 text-lg cursor-pointer hover:bg-gray-200 rounded" onClick={onClickHandler}>
+    <div className="relative">
+      <div className="p-1 text-lg cursor-pointer hover:bg-gray-200 rounded" onClick={onClickHandler}>
         <FiMoreHorizontal />
       </div>
       {isOpened && (
-        <div className="absolute bg-white w-20 border-2 border-gray-300 rounded-sm p-1 translate-y-10 -translate-x-6">
+        <div className="absolute bg-white w-20 border-2 border-gray-300 rounded-sm p-1 translate-y-1 -translate-x-14">
           <button type="button" className="btn-more" onClick={(e) => editHandler(e)}>Edit</button>
           <button type="button" className="btn-more" onClick={(e) => deleteHandler(e)}>Delete</button>
         </div>
       )}
-    </>
+    </div>
   );
 }
 
