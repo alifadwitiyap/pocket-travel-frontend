4f063c564f82ec4473d309224bbed9e91628b4a1
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2022-04-06 16:54:30 +0700

    Add HomePage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Register from "./pages/Register";
 import Main from "./pages/Main";
 import DiaryPage from "./pages/DiaryPage";
 import PlanPage from "./pages/PlanPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<Main />}>
-          <Route index element={<h1>home</h1>} />
+          <Route index element={<HomePage />} />
           <Route path="diary" element={<DiaryPage />} />
           <Route path="checklist" element={<h1>checklist</h1>} />
           <Route path="plan" element={<PlanPage />} />
