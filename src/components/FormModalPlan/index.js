ce7f712c0aeeebe388588a1b8312f43c468e286f
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2022-04-09 23:44:40 +0700

    Add FormModalPlan component (layout + client logic)

---
+++ src/components/FormModalPlan/index.js
@@ -0,0 +1,200 @@
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { HiOutlineTrash } from 'react-icons/hi';
+import DatePicker from 'react-datepicker';
+import TimePicker from '../TimePicker';
+import CountryPicker from '../CountryPicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import './index.css';
+import axios from 'axios';
+
+function FormModalPlan({ planId, action, setModal }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    date: new Date(),
+    country: '',
+    name: '',
+  });
+  const [schedule, setSchedule] = useState([]);
+
+  const onChange = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const addScheduleItem = () => {
+    setSchedule((prev) => [
+      ...prev,
+      {
+        time: '00:00',
+        activity: '',
+      },
+    ]);
+  };
+
+  const deleteScheduleItem = (idx) => {
+    setSchedule((prev) => [...prev.slice(0, idx), ...prev.slice(idx + 1)]);
+  };
+
+  const tableInputOnChange = (e, idx) => {
+    setSchedule((prev) => {
+      const cp = [...prev];
+      cp[idx][e.target.name] = e.target.value;
+      return cp;
+    });
+  };
+
+  useEffect(() => {
+    // get user id from redux, and save it
+    // if action is edit, then reqeust to backend so can fill form
+    if (action === 'edit') {
+      setIsLoading(true);
+      const id = 'plan-ay11AWX74ppKXbxr';
+      axios
+        .get(`https://pocket-travel-api.herokuapp.com/api/plan/${id}/detail`, {
+          headers: {
+            Authorization:
+              'Bearer 6250ef90d621d518a6228c12|nVnxTFPho9cOSClvh2Cy5AERTcoud6sBE468IKlx',
+          },
+        })
+        .then((res) => {
+          const plan = res.data.plan;
+          setFormData({
+            date: new Date(plan.date),
+            country: plan.country,
+            name: plan.name,
+          });
+          setSchedule(plan.schedule);
+          setIsLoading(false);
+        })
+        .catch((err) => err);
+    }
+  }, [planId, action]);
+
+  return (
+    <>
+      <div
+        className="fixed top-0 left-0 h-screen w-screen bg-black opacity-75"
+        onClick={() => setModal(false)}
+      ></div>
+      <div className="fixed left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
+        <form className="bg-white py-6 px-10 rounded flex flex-col gap-2 md:flex-row md:gap-10">
+          {isLoading ? (
+            <h1>loading...</h1>
+          ) : (
+            <>
+              <div className="flex flex-col justify-between items-start">
+                <div className="min-w-[300px]">
+                  <h1 className="font-bold text-2xl mb-4">Plan</h1>
+                  <div className="mb-4">
+                    <label className="block font-bold mb-1">
+                      Date<span className="text-red-700">*</span>
+                    </label>
+                    <DatePicker
+                      className="appearance-none rounded relative block w-full px-3 py-2 border border-black placeholder-gray-300 text-black focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                      selected={formData.date}
+                      onChange={(date) =>
+                        setFormData((prev) => ({ ...prev, date }))
+                      }
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label className="block font-bold mb-1">
+                      Country<span className="text-red-700">*</span>
+                    </label>
+                    <CountryPicker
+                      country={formData.country}
+                      onChange={(country) =>
+                        setFormData((prev) => ({ ...prev, country }))
+                      }
+                    />
+                  </div>
+                  <div className="mb-6">
+                    <label className="block font-bold mb-1">
+                      Plan Name<span className="text-red-700">*</span>
+                    </label>
+                    <input
+                      className="appearance-none rounded relative block w-full px-3 py-2 border border-black placeholder-gray-300 text-black focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                      type="text"
+                      name="name"
+                      placeholder="ex: Bali Trip Plans"
+                      value={formData.name}
+                      onChange={onChange}
+                    />
+                  </div>
+                </div>
+                <button
+                  type="button"
+                  className="px-4 py-1 border border-transparent rounded-lg shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Create +
+                </button>
+              </div>
+              <div className="w-[2px] h-auto bg-gray-300"></div>
+              <div>
+                <div className="flex items-center mb-4">
+                  <h1 className="grow font-bold">Schedule</h1>
+                  <button
+                    type="button"
+                    className="p-2 border-transparent rounded-lg shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-gray-100 border border-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    onClick={addScheduleItem}
+                  >
+                    Add Item +
+                  </button>
+                </div>
+                <table className="form-modal border-collapse border border-gray-700">
+                  <thead>
+                    <tr>
+                      <th>Activity</th>
+                      <th>Action</th>
+                      <th>Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {[...Array(schedule.length)].map((_, index) => (
+                      <tr key={index}>
+                        <td className="p-2">
+                          <TimePicker
+                            value={schedule[index].time || '00:00'}
+                            onChange={(e) => tableInputOnChange(e, index)}
+                          />
+                        </td>
+                        <td className="p-1">
+                          <input
+                            className="border"
+                            type="text"
+                            name="activity"
+                            value={schedule[index].activity || ''}
+                            onChange={(e) => tableInputOnChange(e, index)}
+                          />
+                        </td>
+                        <td className="p-2">
+                          <button
+                            type="button"
+                            onClick={() => deleteScheduleItem(index)}
+                            className="bg-red-600 hover:bg-red-700 p-2 rounded text-white"
+                          >
+                            <HiOutlineTrash />
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </>
+          )}
+        </form>
+      </div>
+    </>
+  );
+}
+
+FormModalPlan.propTypes = {
+  action: PropTypes.oneOf(['create', 'edit']),
+  setModal: PropTypes.func.isRequired,
+};
+
+export default FormModalPlan;
