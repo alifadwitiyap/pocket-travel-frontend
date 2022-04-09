acdc7ee25703b4133c71a6cba4625944e3f744ea
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2022-04-10 00:18:42 +0700

    Seperate getBackendUrl as a utility function in its own file

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -4,6 +4,7 @@ import { useDispatch } from "react-redux";
 import { login } from "../../features/authSlice";
 import { useNavigate } from "react-router-dom";
 import bgLogin from "../../assets/login_background.jpg";
+import getBackendUrl from "../../utils/getBackendUrl";
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -20,7 +21,7 @@ export default function Login() {
 
     const dataUser = await axios
       .post(
-        "https://pockettravel-api.herokuapp.com/api/login",
+        `${getBackendUrl()}/login`,
         JSON.stringify(dataFormUser)
       )
       .then((response) => response.data)
---
+++ src/utils/getBackendUrl.js
@@ -0,0 +1,3 @@
+const getBackendUrl = () => `https://pockettravel-api.herokuapp.com/api`;
+
+export default getBackendUrl;
