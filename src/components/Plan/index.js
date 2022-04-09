1187dd86b29348f6ef9f4e6c74011130d0f93bf9
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2022-04-10 06:38:04 +0700

    Add edit and delete handlers at Plan component

--- src/components/Plan/index.js
+++ src/components/Plan/index.js
@@ -3,11 +3,12 @@ import PropTypes from "prop-types";
 import ReactCountryFlag from "react-country-flag";
 import More from "../More";
 import exampleData from "./exampleData";
+import onlyDate from "../../utils/onlyDate";
 
-function Plan({ data }) {
-  const { name, date, country, schedule } = data;
+function Plan({ data, onEdit, onDelete }) {
+  const { plan_id, name, date, country, schedule } = data;
   return (
-    <div className="relative bg-white border-2 border-gray-300 p-5 w-4/5 sm:w-2/3 lg:w-1/3">
+    <div className="mb-6 relative bg-white border-2 border-gray-300 p-5 w-4/5 sm:w-2/3 lg:w-1/3">
       <div className="mb-2 flex items-center justify-between">
         <ReactCountryFlag
           countryCode={country}
@@ -15,7 +16,7 @@ function Plan({ data }) {
           style={{ fontSize: "18px" }}
         />
         <h2 className="grow font-bold">{name}</h2>
-        <p className="text-gray-700">{date}</p>
+        <p className="text-gray-700">{onlyDate(date)}</p>
       </div>
       <div className="text-sm grid grid-cols-[4ch_8px_1fr] gap-x-5 gap-y-1">
         {schedule.map((item, index) => (
@@ -51,8 +52,7 @@ function Plan({ data }) {
         ))}
       </div>
       <div className="absolute right-5 bottom-5">
-        {/* TODO: Add handlers for edit and delete */}
-        <More />
+        <More editHandler={() => onEdit(plan_id)} deleteHandler={() => onDelete(plan_id)} />
       </div>
     </div>
   );
@@ -60,10 +60,14 @@ function Plan({ data }) {
 
 Plan.defaultProps = {
   data: exampleData,
+  onEdit: () => {},
+  onDelete: () => {},
 };
 
 Plan.propTypes = {
   data: PropTypes.object,
+  onEdit: PropTypes.func,
+  onDelete: PropTypes.func,
 };
 
 export default Plan;
