d9333d88993354acc431dbb7d19aca821e161d80
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2022-04-10 22:55:18 +0700

    Update navbar to conditionally adapt to authenticated state

--- src/components/NavbarAvatar/index.js
+++ src/components/NavbarAvatar/index.js
@@ -6,9 +6,12 @@ function NavbarAvatar() {
   const token = useSelector((state) => state.auth.token);
 
   return (
-    <div className="relative">
+    <div className={`relative ${!token && "ml-8"}`}>
       {!token ? (
-        <Link className="login" to="/login">Login</Link>
+        <>
+          <Link className="login mr-6" to="/login">Login</Link>
+          <Link className="register" to="/register">Sign Up</Link>
+        </>
       ) : (
         <Link className="logout" to="/login">Logout</Link>
       )}
