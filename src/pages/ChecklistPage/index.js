8ba502cda7e477d3ecd1ec5dcd97fa51d7a033e3
Mar'i Fauzan <marifauzan20@gmail.com>
2022-04-12 12:42:03 +0700

    Add function Create in Pages Checklist

--- src/pages/ChecklistPage/index.js
+++ src/pages/ChecklistPage/index.js
@@ -1,15 +1,37 @@
-import { useEffect, useState } from 'react';
-import { HiOutlinePencil, HiOutlineTrash } from 'react-icons/hi';
-import dummyData from './dummyData';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { HiOutlinePencil, HiOutlineTrash } from "react-icons/hi";
+import { useSelector } from "react-redux";
+import getBackendUrl from "../../utils/getBackendUrl";
 
 function ChecklistPage() {
   const [listItem, setListItem] = useState([]);
+  const [name, setName] = useState("");
+  const { user_id, token } = useSelector((state) => state.auth);
 
   useEffect(() => {
-    setListItem(dummyData);
-  }, []);
+    axios
+      .get(`${getBackendUrl()}/checklist/${user_id}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => setListItem(response.data.itemList))
+      .catch((error) => console.log(error));
+    // setListItem(dummyData);
+  }, [user_id, token]);
 
   // TODO: update this when backend is ready
+  const handleAddItem = async () => {
+    await axios.post(
+      `${getBackendUrl()}/checklist/${user_id}`,
+      {
+        name: name,
+      },
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+  };
+
   const toggleCheckbox = (id) => console.log(id);
   const editHandler = (id) => console.log(`edit ${id}`);
   const deleteHandler = (id) => console.log(`edit ${id}`);
@@ -29,8 +51,12 @@ function ChecklistPage() {
             type="text"
             className="flex-[4_1_0%] appearance-none rounded-2xl relative block w-full px-3 py-2 border border-black placeholder-gray-300 text-black focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
             placeholder="Item name"
+            onChange={(e) => setName(e.target.value)}
           />
-          <button className="flex-[1_1_0%] px-4 py-1 border border-transparent rounded-2xl shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          <button
+            className="flex-[1_1_0%] px-4 py-1 border border-transparent rounded-2xl shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            onClick={handleAddItem}
+          >
             Add Item +
           </button>
         </div>
@@ -40,25 +66,25 @@ function ChecklistPage() {
         </p>
 
         {listItem.map((item) => (
-          <div key={item.id} className="flex gap-4 py-1 items-center">
+          <div key={item.item_id} className="flex gap-4 py-1 items-center">
             <input
               type="checkbox"
               checked={item.is_checked}
-              onChange={() => toggleCheckbox(item.id)}
+              onChange={() => toggleCheckbox(item.item_id)}
             />
             <p>{item.name}</p>
             <div className="grow h-[1px] bg-gray-500"></div>
             <button
               className="text-lg text-indigo-600 hover:text-indigo-700"
               type="button"
-              onClick={() => editHandler(item.id)}
+              onClick={() => editHandler(item.item_id)}
             >
               <HiOutlinePencil />
             </button>
             <button
               className="text-lg text-red-600 hover:text-red-700"
               type="button"
-              onClick={() => deleteHandler(item.id)}
+              onClick={() => deleteHandler(item.item_id)}
             >
               <HiOutlineTrash />
             </button>
