f7ee86737264c421409eeeb4c0ef5db72fb09884
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2022-04-12 17:14:06 +0700

    Add fetch diary to DiaryPage and create diary functionality

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -3,11 +3,13 @@ import { FaRegUserCircle } from 'react-icons/fa';
 import ReactCountryFlag from 'react-country-flag';
 import More from '../More';
 import exampleData from './exampleData';
+import onlyDate from '../../utils/onlyDate';
 
 function Post({ data, own }) {
-  const { user, country, location, image, caption, date } = data;
+  const { user, country, location, image, caption, created_at } = data;
+
   return (
-    <div className="relative bg-white border-2 border-gray-300 w-4/5 sm:w-2/3 md:w-1/2 lg:w-1/3">
+    <div className="relative bg-white border-2 mb-6 border-gray-300 w-4/5 sm:w-2/3 md:w-1/2 lg:w-1/3">
       <div className="flex items-center justify-between p-4">
         <FaRegUserCircle className="text-4xl mr-2" />
         <div className="grow">
@@ -22,7 +24,7 @@ function Post({ data, own }) {
       <img src={image} alt="helo" />
       <div className="p-4">
         <p className="text-sm mb-2">{caption}</p>
-        <p className="text-xs text-gray-700">{date}</p>
+        <p className="text-xs text-gray-700">{onlyDate(created_at)}</p>
       </div>
     </div>
   );
--- src/pages/DiaryPage/index.js
+++ src/pages/DiaryPage/index.js
@@ -1,32 +1,64 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import FormModalDiary from '../../components/FormModalDiary';
 import Post from '../../components/Post';
+import axios from 'axios';
+import getBackendUrl from '../../utils/getBackendUrl';
+import { useDispatch, useSelector } from 'react-redux';
+import { setDiary } from '../../features/diarySlice';
 
 function DiaryPage() {
+  const { token } = useSelector((state) => state.auth);
+  const { diary } = useSelector((state) => state.diary);
+  const dispatch = useDispatch();
+
   const [{ isModalOpened, modalAction, diaryId }, setModalState] = useState({
     isModalOpened: false,
     modalAction: '',
     diaryId: '',
   });
+  
+  const fetchDiaries = useCallback(async () => {
+    const diaries = await axios.get(`${getBackendUrl()}/diary`,
+        { headers: { Authorization: `Bearer ${token}` } }
+      ).then((res) => res.data);
+    dispatch(setDiary(diaries.data));
+  }, [token, dispatch]);
+  
+  useEffect(() => {
+    if (diary.length === 0) {
+      fetchDiaries();
+    }
+  }, [diary.length, fetchDiaries]);
 
   return (
     <div className="flex flex-col items-center mt-6">
       <div className="relative border-2 border-gray-300 p-5 w-4/5 sm:w-2/3 md:w-1/2 lg:w-1/3 mb-6">
         <div className="flex justify-between items-center">
           <h1 className="text-2xl font-bold">Diary</h1>
-          <button className="items-center px-4 py-1 border border-transparent rounded-2xl shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          <button
+            type="button"
+            className="items-center px-4 py-1 border border-transparent rounded-2xl shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            onClick={() => {
+              setModalState({
+                isModalOpened: true,
+                modalAction: 'create',
+                diaryId: ''
+              })
+            }}
+          >
             Create +
           </button>
         </div>
       </div>
-      <button type="button" onClick={() => {
-        setModalState({
-          isModalOpened: true,
-          modalAction: 'create',
-          diaryId: ''
-        })
-      }}>open modal</button>
-      <Post own />
+
+      {diary.map((diary) => (
+        <Post
+          key={diary._id}
+          data={diary}
+          own
+        />
+      ))}
+
       {isModalOpened && (
         <FormModalDiary
           planId={diaryId}
@@ -34,6 +66,7 @@ function DiaryPage() {
           setModal={(open) =>
             setModalState((prev) => ({ ...prev, isModalOpened: open }))
           }
+          reset={fetchDiaries}
         />
       )}
     </div>
