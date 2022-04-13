a7daccd052c837cbd7c4b9abf9ff9537cdf7b541
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2022-04-13 22:35:49 +0700

    Add fetch public diaries and display it onto Home page

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,4 +1,22 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Post from '../../components/Post';
+import getBackendUrl from '../../utils/getBackendUrl';
+
 function HomePage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [publicDiaries, setPublicDiaries] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get(`${getBackendUrl()}/public`)
+      .then((res) => {
+        setPublicDiaries(res.data.data);
+        setIsLoading(false);
+      })
+  }, []);
+
   return (
     <div className="flex flex-col items-center mt-6">
       <div className="relative border-2 border-gray-300 p-5 w-4/5 sm:w-2/3 md:w-1/2 lg:w-1/3 mb-6">
@@ -6,6 +24,7 @@ function HomePage() {
           <h1 className="text-2xl font-bold">Home</h1>
         </div>
       </div>
+      {isLoading ? "loading..." : publicDiaries.map((diary) => <Post key={diary._id} data = {diary} />)}
     </div>
   );
 }
